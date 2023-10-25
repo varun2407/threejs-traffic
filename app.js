1f40b4b1e8c5f485379c1a814c685232835a92aa
gaiborjosue <gaiborjimenezjosue@gmail.com>
2023-10-24 23:42:40 -0400

    fix(app): Fixed scene and mesh object issue

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
 import * as THREE from "three";
 import { AmbientLight } from "three";
+import Car from "./car.js"
 
-const scene = new THREE.Scene();
+window.onload = () => {
+  const scene = new THREE.Scene();
 
-const playerCar = Car();
-scene.add(playerCar);
+  const playerCar = Car();
+  scene.add(playerCar);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
-scene.add(AmbientLight)
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.6);
+  scene.add(ambientLight)
 
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
-dirLight.position.set(100, -300, 400);
-// x,y,z coordinates for light. x is the lowest(front of the car is darkest.) z is the highest value (top of the car is brightest)
-scene.add(dirLight);
+  const dirLight = new THREE.DirectionalLight(0xffffff, 0.6);
+  dirLight.position.set(100, -300, 400);
+  // x,y,z coordinates for light. x is the lowest(front of the car is darkest.) z is the highest value (top of the car is brightest)
+  scene.add(dirLight);
 
 
-// Orthographic camera:
-const aspectRatio = window.innerWidth / window.innerHeight;
-const cameraWidth = 150;
-const cameraHeight = cameraWidth / aspectRatio;
+  // Orthographic camera:
+  const aspectRatio = window.innerWidth / window.innerHeight;
+  const cameraWidth = 150;
+  const cameraHeight = cameraWidth / aspectRatio;
 
-const camera = new THREE.OrthographicCamera(
-  cameraWidth / -2, //left
-  cameraWidth / 2, //right
-  cameraHeight / 2, //top
-  cameraHeight / -2, //bottom
-  0, //near plane 
-  1000 //far plane
-);
-camera.position.set(200, -200, 300);
-camera.up.set(0,0,1);
-camera.lookAt(0, 0, 0);
+  const camera = new THREE.OrthographicCamera(
+    cameraWidth / -2, //left
+    cameraWidth / 2, //right
+    cameraHeight / 2, //top
+    cameraHeight / -2, //bottom
+    0, //near plane 
+    1000 //far plane
+  );
+  camera.position.set(200, -200, 300);
+  camera.up.set(0,0,1);
+  camera.lookAt(0, 0, 0);
 
-// renderer
-const renderer = new THREE.WebGLRenderer( {antialias: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.render(scene, camera);
-document.body.appendChild(renderer.domElement);
+  // renderer
+  const renderer = new THREE.WebGLRenderer( {antialias: true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+  document.body.appendChild(renderer.domElement);
+
+}
